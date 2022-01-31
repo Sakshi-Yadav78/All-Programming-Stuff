92ea873c81655654c7163a51c4ad0a38806754ab
Sakshi-Yadav78 <yadavsakshi.2322@gmail.com>
2022-01-31 16:18:50 +0530

    commit

---
+++ Programs/Projects/Quizz_game.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	//These are ten questions set
+	string questions[10] = { 
+		" Who won the Best Footballer 2016 in the World? ",
+		" Who is the UN Secretary General? ",
+		" What was Word of the Year-2016 according to the Dictionary.com? ",
+		" In Bihar, UNESCO has declared which place as a World Heritage Site? ",
+		" What do you call a house made of ice? ",
+		" Where is the first fully digitalized Post Office located? ",
+		" Who developed Integrated Circuit Chip? ",
+		" Which of the following will be used for checking spelling? ",
+		" Which of the following company first created Hard Disk in the world? ",
+		" When was the first motherboard made? "
+		};
+	//These are ten ansers set with their 4 options
+	string options[10] [4] = {
+		{"Stephen Hawking", "JS Kilby", "Ken Olsen","Cristiano Ronaldo"},
+		{"António Guterres","George Harrar","Harlen Anderson","Glenn Rifkin"},
+		{"existential","xenophobia","pandemic","Vax"},
+		{"Archaeological Site of Nalanda Mahavihara","Ajanta Caves","Ellora Caves","Taj Mahal"},
+		{"Tent","Caravan","igloo"," Pukka house"},
+		{"Surat","Ahmedabad","New Delhi","Bangalore"},
+		{"Stephen Hawking"," Isaac Newton","J S Kilby"," Jagdishchandra Vasu"},
+		{"Dictionary Disk","Index Disk"," Directory Disk"," None of the These"},
+		{"IBM","HP","Epson","Samsung"},
+		{"1978","1923","1981","1998"},};
+
+		
+
+
+
+
+
+
+	}
+	return 0;
+}
