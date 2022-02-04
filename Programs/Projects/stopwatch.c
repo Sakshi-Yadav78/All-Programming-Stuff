de9eb054a68b0eb369664f0091c0e4a77896bea7
Sakshi-Yadav78 <yadavsakshi.2322@gmail.com>
2022-02-04 15:09:13 +0530

    right

--- Programs/Projects/stopwatch.c
+++ Programs/Projects/stopwatch.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <conio.h>
+#include <unistd.h>
 
 int main()
 {
+    int hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
+	while(1){
+		printf("\r\t%.2d HOURS: %.2d MINUTES: %.2d SECONDS: %.2d", hours, minutes, 
+	          seconds,milliseconds); 
+			usleep(1000);
+			++milliseconds;
+	    if(milliseconds == 999){
+		   milliseconds = 0;
+		   ++seconds;
+		}
+		if(seconds == 60)
+		{
+			milliseconds = 0;
+			seconds = 0;
+			++minutes;
+		}
+		if(minutes == 60)
+		{
+			milliseconds = 0;
+			seconds = 0;
+			minutes = 0;
+			++hours;
+		}
+		}
 	return 0;
 }
