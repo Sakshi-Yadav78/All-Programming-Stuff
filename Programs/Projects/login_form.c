be0b2beca50eefd13871433848e209a84e0ca5fb
Sakshi-Yadav78 <yadavsakshi.2322@gmail.com>
2022-02-04 12:13:53 +0530

    d

--- Programs/Projects/login_form.c
+++ Programs/Projects/login_form.c
@@ -3,5 +3,24 @@
 
 int main()
 {
+	//Create user_id and password in the form of two variable
+	int user_id1 = 12 , password1 = 56;
+	int user_id2, password2;
+    // Message for user_id
+	printf(" Enter user_id \n");
+	scanf("%d", &user_id2);
+
+	//Message for password
+	printf(" Enter password \n");
+	scanf("%d", &password2);
+
+	if(user_id1==user_id2&&password1==password2)
+	{
+		printf(" True Login ");
+	}
+	else
+    {
+		printf(" Wrong user_id or password!! ");
+	}
 	return 0;
 }
