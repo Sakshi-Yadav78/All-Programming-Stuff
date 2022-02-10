854106b2941e38c063d89d61b303a6e84a56903f
Sakshi-Yadav78 <yadavsakshi.2322@gmail.com>
2022-02-10 11:29:31 +0530

    done

--- Programs/Polymorphism/output.cpp
+++ Programs/Polymorphism/output.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-	unsigned int x = 3;
-	while (x --> = 0)
+	unsigned int x = 3; //unsigned int means is a 32-bit datum that encodes a nonnegative integer in the range [0 to 4294967295]//
+	while (x --> = 0) // --> it acts like a dot operator, try replacing it with '.'//
 	{
-		cout << x;
+		cout << x;  //no statement is given here //
 	}
 	return 0;
 }
